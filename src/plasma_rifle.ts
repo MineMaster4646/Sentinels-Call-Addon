ffdcdbf0f68c3f2ffe3f3c77cb2311a1650eb4bd
AndroAGENT <andrija.misic.yt@gmail.com>
2024-06-12 15:14:54 +0200

    Update plasma_rifle.ts

--- src/plasma_rifle.ts
+++ src/plasma_rifle.ts
@@ -1,12 +1,12 @@
-import { world, Vector, ItemStack, MinecraftEntityTypes } from "@minecraft/server";
+import { world, Vector, ItemStack, MinecraftEntity } from "@minecraft/server";
 
-function firePlasma(player) {
+function firePlasma(player: MinecraftEntity) {
   const plasma = world.getDimension("overworld").spawnEntity("dea:plasma", player.location);
   const viewVector = new Vector(player.viewVector.x, player.viewVector.y, player.viewVector.z);
   plasma.setVelocity(viewVector.scale(2));
 }
 
-world.events.beforeItemUse.subscribe((event) => {
+world.events.beforeItemUse.subscribe((event: { source: MinecraftEntity, item: ItemStack }) => {
   const player = event.source;
   const item = event.item;
 
