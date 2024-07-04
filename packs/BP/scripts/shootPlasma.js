c2b0729e972f6fd4008efc14179b708b0d658260
AndroAGENT <andrija.misic.yt@gmail.com>
2024-07-04 12:49:24 +0200

    RP fix
    Fixed all the RP files (probably)

--- packs/BP/scripts/shootPlasma.js
+++
@@ -1,8 +0,0 @@
-import { world } from "@minecraft/server";
-world.beforeEvents.worldInitialize.subscribe(initEvent => {
-    initEvent.itemComponentRegistry.registerCustomComponent('sc:pr_shoot_on_use', {
-        onUse(arg) {
-            arg.source.addEffect('minecraft:night_vision', 600);
-        }
-    });
-});
